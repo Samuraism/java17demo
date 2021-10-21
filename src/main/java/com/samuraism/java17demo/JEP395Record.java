6b39f16b86873b33dc26116caf5dcea7c91a6d7a
Yusuke Yamamoto <yusuke@mac.com>
2021-10-21 12:55:39 +0900

    JEP 395: Records

--- src/main/java/com/samuraism/java17demo/JEP395Record.java
+++ src/main/java/com/samuraism/java17demo/JEP395Record.java
@@ -9,46 +9,9 @@ import java.util.Objects;
 public class JEP395Record {
     public static void main(String[] args) {
         Person yusuke = new Person("yusuke", 18);
-        System.out.printf("name:%s age:%s%n", yusuke.getName(), yusuke.getAge());
+        System.out.printf("name:%s age:%s%n", yusuke.name(), yusuke.name());
     }
 }
 
-class Person {
-    private final String name;
-    private final int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person that = (Person) o;
-        return age == that.age && Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-               "name='" + name + '\'' +
-               ", age=" + age +
-               '}';
-    }
+record Person(String name, int age) {
 }
